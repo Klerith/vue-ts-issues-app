6629608378a61dd3e309c9aae5303e4138afab11
Fernando Herrera <fernando.herrera85@gmail.com>
2022-11-08 16:31:12 -0500

    Pages y routes

---
+++ src/issues/pages/IssuePage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <span class="text-h2">Issue: </span>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,10 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('src/issues/pages/ListPage.vue') },
+      { path: '/issue/:id', component: () => import('src/issues/pages/IssuePage.vue') },
+    ],
   },
 
   // Always leave this as last one,
